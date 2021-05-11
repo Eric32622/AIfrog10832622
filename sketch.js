fcd55d105aa0874ae8f21ebf6d3d9739b9e1c8e5
Eric32622 <83267918+Eric32622@users.noreply.github.com>
2021-05-11 15:33:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://eric32622.github.io/AIfrog10832622/model.json';
 
 
 function preload() {
